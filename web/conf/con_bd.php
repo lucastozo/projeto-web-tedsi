7627e260947691f62c9db9c7e6785282617d4440
Lucas Tozo <lucastozo7@gmail.com>
2025-08-29 16:28:36 -0400

    add: (web) estrutura inicial

---
+++ web/conf/con_bd.php
@@ -0,0 +1,24 @@
+<?php
+
+define("ENDERECO", "localhost");
+define("USUARIO", "root");
+define("SENHA", "root");
+define("BD_NOME", "pokemon-tedsi");
+define("PORTA", 3306);
+
+$con_bd = false;
+
+try {
+    $con_bd = mysqli_connect(
+        ENDERECO,
+        USUARIO,
+        SENHA,
+        BD_NOME,
+        PORTA
+    );
+} catch (Exception $e) {
+    echo "Erro conectando ao banco de dados!"
+        . " <br>Código do erro: ".$e->getCode();
+}
+
+?>
---
+++ web/pokemon/script/cadastrar.php
@@ -0,0 +1,14 @@
+<!-- 
+Script responsável para cadastrar E atualizar pokemon
+Como diferenciar?
+- Vou preencher todos os campos do página html com informações vindas da URL
+- Uma das informações na URL é uma flag que indica que é para atualizar
+
+E então pegar todos os dados nos campos da página, verificar, e executar a function certa
+
+Idealmente, o trabalho de ajustar as relações das outras entidades serão feitas por triggers
+  -->
+
+<?php
+
+?>
