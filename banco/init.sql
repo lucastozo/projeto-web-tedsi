410dcf6fa94c1cd54c5371cd9297e472b8afe6ca
BachJu <bachjuliana.julianasbach@gmail.com>
2025-09-01 19:19:48 -0400

    CRUD habilidade para a tabela habilidade.

--- banco/init.sql
+++ banco/init.sql
@@ -1,3 +1,4 @@
+-- Create tables
 CREATE TABLE IF NOT EXISTS pokemon(
 id INTEGER AUTO_INCREMENT PRIMARY KEY,
 imagem VARCHAR(255),
@@ -29,7 +30,10 @@ id_pokemon INTEGER,
 FOREIGN KEY (id_tipo) REFERENCES tipo(id),
 FOREIGN KEY (id_pokemon) REFERENCES pokemon(id));
 
+-- Procedures
 DELIMITER //
+
+-- CRUD tipo
 CREATE PROCEDURE novo_tipo(
 IN p_nv_tipo VARCHAR(30)
 )
@@ -75,4 +79,50 @@ END //
 CREATE PROCEDURE listar_tipo()
 BEGIN
 SELECT nome FROM tipo;
+END //
+
+-- CRUD habilidade
+
+CREATE PROCEDURE nova_habilidade(
+IN p_nv_nome VARCHAR(30),
+IN p_nv_descricao VARCHAR(255)
+)
+BEGIN
+DECLARE cnt_habilidade INT;
+SELECT COUNT(*) INTO cnt_habilidade FROM habilidade WHERE nome = p_nv_nome;
+IF cnt_habilidade > 0 THEN
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Essa habilidade já foi cadastrada.';
+END IF;
+INSERT INTO habilidade(nome, descricao) VALUES (p_nv_nome, p_nv_descricao);
+END //
+
+CREATE PROCEDURE atualizar_habilidade(
+IN p_ant_nome VARCHAR(30),
+IN p_nv_nome VARCHAR(30),
+IN p_nv_descricao VARCHAR(255)
+)
+BEGIN
+DECLARE cnt_habilidade INT;
+SELECT COUNT(*) INTO cnt_habilidade FROM habilidade WHERE nome = p_ant_nome;
+IF cnt_habilidade = 0 THEN
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Não existe uma habilidade com esse nome.';
+END IF;
+SET @cnt_habilidade := 0;
+SELECT COUNT(*) INTO cnt_habilidade FROM habilidade WHERE nome = p_nv_nome;
+IF cnt_habilidade > 0 AND p_nv_nome != p_ant_nome THEN
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Já existe uma habilidade com esse nome.';
+END IF;
+UPDATE habilidade SET nome = p_nv_nome, descricao = p_nv_descricao WHERE nome = p_ant_nome;
+END //
+
+CREATE PROCEDURE deletar_habilidade(
+IN p_dlt_habilidade VARCHAR(30)
+)
+BEGIN
+DECLARE cnt_habilidade INT;
+SELECT COUNT(*) INTO cnt_habilidade FROM habilidade WHERE nome = p_dlt_habilidade;
+IF cnt_habilidade = 0 THEN
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Habilidade não cadastrada.';
+END IF;
+DELETE FROM habilidade WHERE nome = p_dlt_habilidade;
 END //
