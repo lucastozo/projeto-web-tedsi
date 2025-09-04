9d43a07ae7d53a15267dbde49a2f5de3d16342a5
Lucas Tozo <lucastozo7@gmail.com>
2025-09-04 00:37:57 -0400

    pokemon listar

--- web/pokemon/listar.php
+++ web/pokemon/listar.php
@@ -1,3 +1,8 @@
+<?php declare(strict_types=1);
+session_start();
+require_once("../utils/mensagem.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -38,7 +43,56 @@
             </div>
         </nav>
         <div class="col-main">
+            <table>
+                <?php
+                    exibir_mensagem();
+                ?>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Altura</th>
+                    <th>Peso</th>
+                    <th>Descrição</th>
+                    <th>Tipos</th>
+                    <th>Habilidades</th>
+                    <th>Ações</th>
+                </tr>
+                <tr>
+                    <?php
+                    require_once("../conf/con_bd.php");
 
+                    if (isset($con_bd)) {
+                        $sql = "SELECT * FROM pokemon ORDER BY id";
+                        $result = mysqli_query($con_bd, $sql);
+                        
+                        if ($result) 
+                        {
+                            if (mysqli_num_rows($result) > 0) 
+                            {
+                                while ($pokemon = mysqli_fetch_assoc($result)) 
+                                {
+                                    echo "<tr>";
+                                    echo "<td>" . htmlspecialchars($pokemon['id']) . "</td>";
+                                    echo "<td>" . htmlspecialchars($pokemon['nome']) . "</td>";
+                                    echo "<td>" . htmlspecialchars($pokemon['altura']) . "</td>";
+                                    echo "<td>" . htmlspecialchars($pokemon['peso']) . "</td>";
+                                    echo "<td>" . htmlspecialchars($pokemon['descricao']) . "</td>";
+                                    echo "<td>" . htmlspecialchars($pokemon['tipos']) . "</td>";
+                                    echo "<td>" . htmlspecialchars($pokemon['habilidades']) . "</td>";
+                                    echo "<td>";
+                                    echo "<a href='visualizar.php?id=" . $pokemon['id'] . "'>Visualizar</a>";
+                                    echo "</td>";
+                                    echo "</tr>";
+                                }
+                            } 
+                            else echo "<tr><td colspan='8' class='msg erro'>Nenhum Pokémon encontrado.</td></tr>";
+                        } 
+                        else echo "<tr><td colspan='8' class='msg erro'>Erro ao buscar Pokémons: " . mysqli_error($con_bd) . "</td></tr>";
+                    } 
+                    else echo "<tr><td colspan='8' class='msg erro'>Erro de conexão com o banco de dados.</td></tr>";
+                    ?>
+                </tr>
+            </table>
         </div>
         <footer class="footer">
             <h3>Trabalho desenvolvido para a disciplina de Tópicos Especiais em Desenvolvimento de Sistemas I</h3>
